fb4c7baa6eec0ed2e8b0ba317063fe55ec7576ac
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-07-24 19:41:16 -0600

    comments

--- python/app.py
+++ python/app.py
@@ -5,15 +5,23 @@ app = Flask(__name__)
 
 try:
 
+#on post, checks data and inserts data into the DB 
+
    @app.post('/api/contact')
    def send_contact():
       
+      #checking send data
          error=api_helper.check_endpoint_info(request.json, ['name', 'email', 'content']) 
          if(error !=None):
             return make_response(jsonify(error), 400)
 
+
+            #connecting to DB and calling the procedure that inputs the data sent
+
          results = dbhelper.run_proceedure('CALL content_send(?,?,?)', 
             [request.json.get('name'),request.json.get('email'),request.json.get('content')])
+
+            #returning the results
 
          if(type(results) == list):
             return make_response(jsonify(results), 200)
@@ -33,13 +41,18 @@ try:
    @app.post('/api/admin')
    
    def create_admin():
+      #checking send data
       
          error=api_helper.check_endpoint_info(request.json, ['username', 'password']) 
          if(error !=None):
             return make_response(jsonify(error), 400)
+            
+            #connecting to DB and calling the procedure that inputs the data sent
 
          results = dbhelper.run_proceedure('CALL create_admin(?,?)', 
                [request.json.get('username'), request.json.get('password')])
+         
+            #returning the results
          
       
          if(type(results) == list):
@@ -59,13 +72,22 @@ try:
    @app.post('/api/admin-login')
    def admin_login():
       
+      #checking send data
+      
          error=api_helper.check_endpoint_info(request.json, ['username','password']) 
          if(error !=None):
             return make_response(jsonify(error), 400)
+         
+            #connecting to DB and calling the procedure that inputs the data sent
+         
+            #also generates a random token that is used in the procedure
 
          token = uuid.uuid4().hex
          results = dbhelper.run_proceedure('CALL admin_login(?,?,?)', 
             [request.json.get('username'), request.json.get('password'), token])
+
+            #returning the results
+
 
          if(type(results) == list):
             return make_response(jsonify(results), 200)
@@ -83,8 +105,11 @@ except:
 
 try:
    @app.post('/api/image_upload')
-   #function gets called on api request
+
    def image_upload():
+      
+      #checking send data
+      
    
       is_valid = api_helper.check_endpoint_info(request.form, ['description', 'type', 'token'])
       
@@ -97,15 +122,19 @@ try:
       
       
       
+      
       results = []
+
+      #iterates over each file, calls the fucntion to save it, then inserts the filename and other data into the DB.
 
       for file in request.files.getlist('uploaded_image'):
          filename = api_helper.save_file(file)
-         # If the filename is None, something has gone wrong
          if filename is None:
                return make_response(jsonify("Sorry, something has gone wrong"), 500)
          
          result = dbhelper.run_proceedure('CALL image_create(?,?,?,?)', [filename, request.form['description'], request.form['type'], request.form['token']])
+         
+         #if the task is sucessful, appends a success message to the empty results list and then returns the results
          
          if (type(results)==list):
                results.append('Success')
@@ -126,12 +155,11 @@ except:
 try:
    @app.get('/api/images')
    def get_images():
-
+      #checking sent data
       is_valid = api_helper.check_endpoint_info(request.args, ['type'])
       if(is_valid != None):
          return make_response(jsonify(is_valid), 400)
-                  
-      # Get the image information from the DB
+
       results = dbhelper.run_proceedure('CALL get_all(?)', [request.args.get('type')])
       # Make sure something came back from the DB that wasn't an error
       if(type(results) != list):
@@ -140,14 +168,12 @@ try:
          return make_response(jsonify("Invalid image id"), 400)
 
             
-
-      # Use the built in flask function send_from_directory
-      # First into the images folder, and then use my results from my DB interaction to get the name of the file
       
       images = []
 
-      # Iterate over each result and create an image object
+      # Iterates over each result and create an image object
       for result in results:
+         
          image = {
             
             'file_name': result['file_name'],
@@ -156,8 +182,11 @@ try:
             'type':  result['type']
          }
 
+         #pushes the created object into the images list
+
          images.append(image)
     
+    #returns the results
       return make_response(jsonify(images), 200)
 
 except TypeError:
